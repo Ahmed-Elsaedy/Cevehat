faed3095636b12e23a473c6e8c4f2b6e378c77d5
asmaashehab <semsema.deutsch@gmail.com>
2018-05-26 15:17:45 +0200

    education , career objective and job title classes

---
+++ Cevehat.Mvc/Models/careerObjective.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cevehat.Mvc.Models
+{
+    public class careerObjective
+    {
+        public int careerId { get; set; }
+        public string careerText { get; set; }
+        
+    }
+}
---
+++ Cevehat.Mvc/Models/education.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cevehat.Mvc.Models
+{
+    public class education
+    {
+        
+        public int educationId { get; set; }
+
+        public decimal GPA { get; set; }
+
+        public DateTime GradYear { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string OrganizationName { get; set; }
+
+        public int userId { get; set; }
+
+    }
+}
