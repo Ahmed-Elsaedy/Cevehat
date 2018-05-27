ba319184dacf905121f74e9cdf13bbfe6b31d61b
Ahmed Elsaedy <ahmedelsaedy92@hotmail.com>
2018-05-27 11:35:31 +0200

    Moving Models Classes

---
+++ Cevehat.Web/Models/InterviewQustion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cevehat.Mvc.Models
+{
+    public class InterviewQustion
+    {
+        public int QID { get; set; }
+        public string Qbody { get; set; }
+        public string QAns { get; set; }
+        public int jobTitleID { get; set; }
+
+    }
+}
---
+++ Cevehat.Web/Models/Skills.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cevehat.Mvc.Models
+{
+    public class Skill
+    {
+        public int Skill_Id { get; set; }
+        public string Skill_name { get; set; }
+        public List<Study_Links> study_Links { get; set; }
+    }
+
+    public class Study_Links
+    {
+        public int SL_id { get; set; }
+        public string link { get; set; }
+        public int Skill_Id { get; set; }
+        public Skill skil { get; set; }
+
+    }
+}
