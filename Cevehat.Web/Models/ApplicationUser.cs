5dfd1ff361c468d96272da90fd675e3606e7afa0
fahemasaid <fahema_abdelmonsf@yahoo.com>
2018-05-28 06:11:43 +0200

    CRUD_of_Skills

--- Cevehat.Web/Models/ApplicationUser.cs
+++ Cevehat.Web/Models/ApplicationUser.cs
@@ -13,7 +13,7 @@ namespace Cevehat.Web.Models
         public MaritalStutes MaritaSutes { get; set; }
         public MilitaryStatus MilitaryStatus { get; set; }
         public bool Gender { get; set; }
-        public virtual List<Skill> TecSkills { get; set; }
+        public virtual List<Skills> TecSkills { get; set; }
         public virtual List<SoftSkill> SoftSkills { get; set; }
         public virtual List<Certification> Certifications { get; set; }
         public virtual List<Experince> Experinces { get; set; }
