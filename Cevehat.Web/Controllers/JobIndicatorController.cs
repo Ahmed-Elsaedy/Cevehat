6a6a3e8dc56e0343e429e93a3bb2a0405ef5ebb4
mrwan202081@gmail.com <mrwan hossam@MRWAN_HOSSAM>
2018-06-02 13:28:39 +0200

    Job Indecator is Done and skill With job title controler

---
+++ Cevehat.Web/Controllers/JobIndicatorController.cs
@@ -0,0 +1,36 @@
+﻿using Cevehat.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cevehat.Web.Controllers
+{
+    public class JobIndicatorController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: JobIndicator
+        public ActionResult Index()
+        {
+            List<JobVacancie> jobVacancies = db.JobVacancie.ToList<JobVacancie>();
+
+            //var qurRes =  (from vac in jobVacancies
+            //group vac by vac.JobTitleID into g
+            //select g).ToList<JobVacancie>().OrderBy(x=> x.JobTitleID)  ;
+
+            List<res> qurRes = jobVacancies.GroupBy(x => x.JobTitle).OrderByDescending(z => z.Count()).Select(y => new res() { JobTitle = y.Key, count = y.Count() }).Take(2).ToList<res>();
+            ViewBag.qurRes = qurRes;
+            //qurRes[0].jobtitle.JobName
+            return View();
+        }
+
+
+    }
+    public class res
+    {
+        public int count { get; set; }
+        public JobTitle JobTitle { get; set; }
+    }
+}
---
+++ Cevehat.Web/Controllers/JobTitles_SkillsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cevehat.Web.Models;
+
+namespace Cevehat.Web.Controllers
+{
+    public class JobTitles_SkillsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: JobTitles_Skills
+        public ActionResult Index()
+        {
+            var jobTitles_Skills = db.JobTitles_Skills.Include(j => j.jobTitle).Include(j => j.skill);
+            return View(jobTitles_Skills.ToList());
+        }
+
+        // GET: JobTitles_Skills/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobTitles_Skills jobTitles_Skills = db.JobTitles_Skills.Find(id);
+            if (jobTitles_Skills == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobTitles_Skills);
+        }
+
+        // GET: JobTitles_Skills/Create
+        public ActionResult Create()
+        {
+            ViewBag.JbTitle_ID = new SelectList(db.JobTitle, "JobId", "JobName");
+            ViewBag.Skill_ID = new SelectList(db.Skill, "Skill_Id", "Skill_name");
+            return View();
+        }
+
+        // POST: JobTitles_Skills/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Skill_ID,JbTitle_ID")] JobTitles_Skills jobTitles_Skills)
+        {
+            if (ModelState.IsValid)
+            {
+                db.JobTitles_Skills.Add(jobTitles_Skills);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.JbTitle_ID = new SelectList(db.JobTitle, "JobId", "JobName", jobTitles_Skills.JbTitle_ID);
+            ViewBag.Skill_ID = new SelectList(db.Skill, "Skill_Id", "Skill_name", jobTitles_Skills.Skill_ID);
+            return View(jobTitles_Skills);
+        }
+
+        // GET: JobTitles_Skills/Edit/5
+        public ActionResult Edit(int? Jobid, int? Skillid)
+        {
+            if (Skillid == null || Jobid == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobTitles_Skills jobTitles_Skills = db.JobTitles_Skills.Where(x => x.JbTitle_ID == Jobid && x.Skill_ID == Skillid).FirstOrDefault();
+            if (jobTitles_Skills == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.JbTitle_ID = new SelectList(db.JobTitle, "JobId", "JobName", jobTitles_Skills.JbTitle_ID);
+            ViewBag.Skill_ID = new SelectList(db.Skill, "Skill_Id", "Skill_name", jobTitles_Skills.Skill_ID);
+            return View(jobTitles_Skills);
+        }
+
+        // POST: JobTitles_Skills/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Skill_ID,JbTitle_ID")] JobTitles_Skills jobTitles_Skills)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobTitles_Skills).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.JbTitle_ID = new SelectList(db.JobTitle, "JobId", "JobName", jobTitles_Skills.JbTitle_ID);
+            ViewBag.Skill_ID = new SelectList(db.Skill, "Skill_Id", "Skill_name", jobTitles_Skills.Skill_ID);
+            return View(jobTitles_Skills);
+        }
+
+        // GET: JobTitles_Skills/Delete/5
+        public ActionResult Delete(int? Jobid, int? Skillidid)
+        {
+            if (Jobid == null || Skillidid == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobTitles_Skills jobTitles_Skills = db.JobTitles_Skills.Where(js => js.JbTitle_ID == Jobid && js.Skill_ID == Skillidid).FirstOrDefault();
+            if (jobTitles_Skills == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobTitles_Skills);
+        }
+
+        // POST: JobTitles_Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            JobTitles_Skills jobTitles_Skills = db.JobTitles_Skills.Find(id);
+            db.JobTitles_Skills.Remove(jobTitles_Skills);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
