e3231a4c34c482acc41acca2335f5b69ef10d7b6
asmaashehab <semsema.deutsch@gmail.com>
2018-06-05 22:14:54 +0200

    "About" link in _ProfilLayout

--- Cevehat.Web/App_Start/RouteConfig.cs
+++ Cevehat.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Cevehat.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
