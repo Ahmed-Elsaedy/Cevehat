1f191120979a37ca417522f947632722daf5d47a
Ahmed Elsaedy <ahmedelsaedy92@hotmail.com>
2018-06-05 16:29:53 +0200

    Login and Register

--- Cevehat.Web/Models/Identity/AccountViewModels.cs
+++ Cevehat.Web/Models/Identity/AccountViewModels.cs
@@ -79,6 +79,9 @@ namespace Cevehat.Web.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 
     public class ResetPasswordViewModel
