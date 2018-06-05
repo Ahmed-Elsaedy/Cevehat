3bb74ca45d67c483f20f2f4c6ef3885121940776
Ahmed Elsaedy <ahmedelsaedy92@hotmail.com>
2018-06-05 12:51:19 +0200

    Register

--- Cevehat.Web/Controllers/ProfileController.cs
+++ Cevehat.Web/Controllers/ProfileController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Cevehat.Web.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,15 +14,127 @@ namespace Cevehat.Web.Controllers
 {
     public class ProfileController : Controller
     {
-        // GET: Profile
-        public ActionResult Index()
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        private ApplicationRoleManager _roleManager;
+
+        public ApplicationRoleManager RoleManager
         {
-            return View();
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
         }
 
-        public ActionResult EditAboutPartial()
+        public ProfileController()
         {
-            return PartialView();
+
+        }
+        public ProfileController(ApplicationUserManager userManager, ApplicationSignInManager signInManager,
+             ApplicationRoleManager roleManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new ProfileIndexViewModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(ProfileIndexViewModel model, string returnUrl)
+        {
+            model.ValidationForLogin = true;
+            if (!ModelState.IsValidField("Email") || !ModelState.IsValidField("Password"))
+                return View("Index", model);
+
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, false, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    ModelState.AddModelError("", "Your account is locked out");
+                    return View("Index", model);
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View("Index", model);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(ProfileIndexViewModel model)
+        {
+            model.ValidationForLogin = false;
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //var currentUser = UserManager.FindByName(user.UserName);
+                    //var roleresult = await UserManager.AddToRoleAsync(currentUser.Id, model.Role);
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error);
+            }
+            var roles = RoleManager.Roles.ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
         }
     }
 }
