40623d83ff009d980427a532972ce38cd96e4739
mrwan202081@gmail.com <mrwan hossam@MRWAN_HOSSAM>
2018-06-18 19:32:31 +0200

    Edite Explore Your JobTitle

--- Cevehat.Web/Controllers/ExploreController.cs
+++ Cevehat.Web/Controllers/ExploreController.cs
@@ -26,21 +26,32 @@ namespace Cevehat.Web.Controllers
             ApplicationUser existsuser = db.Users.Find(User.Identity.GetUserId());
             List<TitlePer> AllTitlesToFit = new List<TitlePer>();
             List<Skill> usertecskills = existsuser.TecSkills;
+            var listSkillsIds = usertecskills.Select(x => x.Skill_Id).ToList();
+
             List<JobTitle> jobtitles = (from titles in db.JobTitle
                                         where titles.SkillCount > 0
                                         select titles).ToList();
+
             foreach (JobTitle Title in jobtitles)
             {
-                var listSkillsIds = usertecskills.Select(x => x.Skill_Id).ToList();
-                var titleCount = (from titleskill in Title.JobTitles_Skills
-                                  where listSkillsIds.Contains(titleskill.skill.Skill_Id)
-                                  select titleskill.Skill_ID).ToList().Count();
-                decimal Pers = (titleCount / Title.SkillCount) * 100;
+                decimal TitleTotalWeight = Title.JobTitles_Skills.Sum(x => x.Weight);
+                decimal userTotalweight = 0;
+                foreach (JobTitles_Skills jobskill in Title.JobTitles_Skills)
+                {
+                    User_Skills user_Skills = db.User_Skills.Where(x => x.SkillID == jobskill.Skill_ID && x.UserId == existsuser.Id).FirstOrDefault();
+                    if (listSkillsIds.Contains(jobskill.skill.Skill_Id))
+                    {
+                        userTotalweight += ((decimal)user_Skills.Weight / 10) * ((decimal)jobskill.Weight / 10);
+                    }
+                }
+                
+                decimal Pers = (userTotalweight / TitleTotalWeight) * 100;
                 List<Skill> RemaingSkills = (from titleskills in Title.JobTitles_Skills
                                              where !listSkillsIds.Contains(titleskills.skill.Skill_Id)
                                              select titleskills.skill).ToList();
                 AllTitlesToFit.Add(new TitlePer() { JobTitle = Title, per = Math.Round(Pers, 2), RemaingSkills = RemaingSkills });
             }
+
             AllTitlesToFit = AllTitlesToFit.OrderBy(x => x.RemaingSkills.Count).OrderByDescending(y => y.per).ToList();
             return View(AllTitlesToFit);
         }
