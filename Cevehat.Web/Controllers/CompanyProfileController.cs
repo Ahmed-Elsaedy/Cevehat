6ef5e4f4378c15e7d98c4f067f952ae9852d563d
emanhabbas00 <emanhabbas00@gmail.com>
2018-06-19 11:42:59 -0700

    hh

--- Cevehat.Web/Controllers/CompanyProfileController.cs
+++ Cevehat.Web/Controllers/CompanyProfileController.cs
@@ -132,7 +132,9 @@ namespace Cevehat.Web.Controllers
             newjob.Date = DateTime.Now;
             newjob.CompanyID = currentComp.CompanyID;
             newjob.Company = currentComp;
-            ////this is shit    
+            newjob.Company = currentComp;
+            ////this is shit  
+
 
             SelectList jobTitles = new SelectList(db.JobTitle.ToList(), "JobId", "JobName");
             ViewBag.JobTitles = jobTitles;
