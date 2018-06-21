a1f204cc848425b3150a7f542b76597f1b5ce0b4
Ahmed Elsaedy <ahmedelsaedy92@hotmail.com>
2018-06-21 13:12:47 +0200

    Test

--- Cevehat.Web/Controllers/CompanyProfileController.cs
+++ Cevehat.Web/Controllers/CompanyProfileController.cs
@@ -121,8 +121,6 @@ namespace Cevehat.Web.Controllers
                 return RedirectToAction("ArchivedJobs");
 
             }
-
-
         }
 
         [Authorize(Roles = "Employer")]
@@ -143,8 +141,6 @@ namespace Cevehat.Web.Controllers
             newjob.CompanyID = currentComp.CompanyID;
             newjob.Company = currentComp;
             newjob.Company = currentComp;
-            ////this is shit  
-
 
             SelectList jobTitles = new SelectList(db.JobTitle.ToList(), "JobId", "JobName");
             ViewBag.JobTitles = jobTitles;
@@ -154,7 +150,6 @@ namespace Cevehat.Web.Controllers
 
             return View(newjob);
         }
-
 
         [HttpPost, Authorize(Roles = "Employer")]
         public ActionResult AddJob(JobVacancie newjob, List<int> Skills)
@@ -170,10 +165,5 @@ namespace Cevehat.Web.Controllers
             db.SaveChanges();
             return RedirectToAction("RecentJobs");
         }
-
-
-
-
-
     }
 }
